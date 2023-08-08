64ecb8ecd8455541cd6181ccbabe23b53aed88be
AdilDevStuff <94475453+AdilDevStuff@users.noreply.github.com>
2023-08-08 22:13:48 +0500

    Update ObjectSpawner.gd

--- Scripts/ObjectSpawner.gd
+++ Scripts/ObjectSpawner.gd
@@ -10,6 +10,8 @@ onready var treeLog = preload("res://Scenes/TreeLog.tscn")
 onready var treeLog2 = preload("res://Scenes/TreeLog2.tscn")
 onready var choppedLog = preload("res://Scenes/ChoppedLog.tscn")
 
+onready var obstaclesGroup = $"../Obstacles"
+
 func _ready():
 	pass
 	
@@ -17,12 +19,12 @@ func _process(delta):
 	spawnDelay.wait_time = rand_range(2,4)
 
 func spawnObstacles():
+	# Spawns Random Obstacles
 	randomize()
 	var obstacleInstance = [treeLog, treeLog2, choppedLog]
 	var obstacle = obstacleInstance[randi() % obstacleInstance.size()]
 	var obj = obstacle.instance()
 	obj.global_position = spawnPos.global_position
-	var obstaclesGroup = get_parent().get_node("Obstacles")
 	obstaclesGroup.add_child(obj)
 	deleteDelay.start()
 	yield(deleteDelay, "timeout")
